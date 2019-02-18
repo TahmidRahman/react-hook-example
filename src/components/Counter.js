f4b65ca260d6a7356c42ca2e83b09ed9851a573b
tahmid131 <tahmid131@gmail.com>
2019-02-19 03:15:59 +0600

    initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Counter.css";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <div className="Container">
+        <h1>
+          {count < 10 ? count.toString().padStart(2, "0") : count.toString()}
+        </h1>
+        <button onClick={() => setCount(count + 1)}>Click to Count</button>
+        <button onClick={() => setCount(0)}>Click to Reset</button>
+      </div>
+      <p>
+        Number and input are inside a Functional Component &nbsp;
+        <code>Counter.js</code>
+      </p>
+    </div>
+  );
+}
